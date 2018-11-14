0c45a90278667d1bb074b799bc70018b13a35002
zzwsfine <lileisifine@aliyun.com>
2018-11-14 14:44:32 +0800

    页面切图和动效完成

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,15 @@
 $(function () {
-
+ var colLi = $('.midColBox li'),closeBtn = $('.closeBtn');
+    colLi.on('click',function () {
+        var liIndex = $(this).index();
+        console.log(liIndex);
+        colLi.css('display','none');
+        $(this).css('display','block').addClass('chosenLi').animate({left:0},500);
+        $('.midProductList').eq(liIndex).fadeIn(500);
+    });
+    closeBtn.on('click',function () {
+        $('.midProductList').fadeOut(500);
+        $('.chosenLi').animate({left:300*$('.midColBox li').index($('.chosenLi'))+'px'}).removeClass('chosenLi');
+        colLi.show();
+    });
 });
