539578f5f020f7af1a9411d3182438d07c876d17
zzwsfine <lileisifine@aliyun.com>
2018-11-14 17:22:50 +0800

    修改动效和hover 透明度

--- javascript/index.js
+++ javascript/index.js
@@ -2,14 +2,21 @@ $(function () {
  var colLi = $('.midColBox li'),closeBtn = $('.closeBtn');
     colLi.on('click',function () {
         var liIndex = $(this).index();
-        console.log(liIndex);
-        colLi.css('display','none');
-        $(this).css('display','block').addClass('chosenLi').animate({left:0},500);
-        $('.midProductList').eq(liIndex).fadeIn(500);
+        if($(this).hasClass('chosenLi')){
+            $('.midProductList').fadeOut(500);
+            $(this).animate({left:300*  liIndex+'px'}).removeClass('chosenLi');
+            colLi.show();
+        }else{
+            colLi.css('display','none');
+            $(this).css('display','block').addClass('chosenLi').animate({left:0},500);
+            $('.midProductList').eq(liIndex).fadeIn(500);
+        }
+
     });
     closeBtn.on('click',function () {
         $('.midProductList').fadeOut(500);
         $('.chosenLi').animate({left:300*$('.midColBox li').index($('.chosenLi'))+'px'}).removeClass('chosenLi');
         colLi.show();
     });
+
 });
