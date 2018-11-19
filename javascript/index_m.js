1e32d4a87f3af9a89fc3c37d8ec6ddb7c9c901f1
zzwsfine <lileisifine@aliyun.com>
2018-11-19 16:31:12 +0800

    移动端年会页面完成

--- javascript/index_m.js
+++ javascript/index_m.js
@@ -1,30 +1,30 @@
-$(function () {
-    var colLi = $('.midColBox li'),closeBtn = $('.closeBtn'),flag = true;
-    colLi.on('click',function () {
-        var liIndex = $(this).index();
-        console.log(1)
-        if(flag){
-            flag = !flag;
-            if($(this).hasClass('unChosenLi')){
-                $(this).removeClass('unChosenLi');
-                $('.unChosenLi').animate({height:0},500);
-                $('.midProductList').eq(liIndex).fadeIn(500);
-            }else{
-                $('.unChosenLi').animate({height:'4.8rem'});
-                $('.midProductList').fadeOut(500);
-                $(this).addClass('unChosenLi');
+    $(function () {
+        var colLi = $('.midColBox li'),closeBtn = $('.closeBtn'),flag = true;
+        colLi.on('click',function () {
+            var liIndex = $(this).index();
+            console.log(1)
+            if(flag){
+                flag = !flag;
+                if($(this).hasClass('unChosenLi')){
+                    $(this).removeClass('unChosenLi');
+                    $('.unChosenLi').slideUp(500);
+                    $('.midProductList').eq(liIndex).slideDown(500);
+                }else{
+                    $('.unChosenLi').slideDown(500);
+                    $('.midProductList').slideUp(500);
+                    $(this).addClass('unChosenLi');
+                }
+                setTimeout(function () {flag = !flag;},500);
             }
-            setTimeout(function () {flag = !flag;},500);
-        }
-    });
-    closeBtn.on('click',function () {
-        if(flag) {
-            flag = !flag;
-            $('.midColBox li:not(.unChosenLi)').addClass('unChosenLi');
-            $('.unChosenLi').animate({height: '4.8rem'},500);
-            $('.midProductList').fadeOut(500);
-            setTimeout(function () {flag = !flag;},500);
-        }
-    });
+        });
+        closeBtn.on('click',function () {
+            if(flag) {
+                flag = !flag;
+                $('.midColBox li:not(.unChosenLi)').addClass('unChosenLi');
+                $('.unChosenLi').slideDown(500);
+                $('.midProductList').slideUp(500);
+                setTimeout(function () {flag = !flag;},500);
+            }
+        });
 
-});
+    });
