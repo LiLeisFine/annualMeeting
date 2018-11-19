a9c5771ba053451dcb4f3b6050ece272b88d6fc0
zzwsfine <lileisifine@aliyun.com>
2018-11-19 17:43:11 +0800

    删除日志输出

--- javascript/index_m.js
+++ javascript/index_m.js
@@ -2,7 +2,6 @@
         var colLi = $('.midColBox li'),closeBtn = $('.closeBtn'),flag = true;
         colLi.on('click',function () {
             var liIndex = $(this).index();
-            console.log(1)
             if(flag){
                 flag = !flag;
                 if($(this).hasClass('unChosenLi')){
@@ -10,8 +9,8 @@
                     $('.unChosenLi').slideUp(500);
                     $('.midProductList').eq(liIndex).slideDown(500);
                 }else{
-                    $('.unChosenLi').slideDown(500);
                     $('.midProductList').slideUp(500);
+                    $('.unChosenLi').slideDown(500);
                     $(this).addClass('unChosenLi');
                 }
                 setTimeout(function () {flag = !flag;},500);
@@ -21,8 +20,8 @@
             if(flag) {
                 flag = !flag;
                 $('.midColBox li:not(.unChosenLi)').addClass('unChosenLi');
-                $('.unChosenLi').slideDown(500);
                 $('.midProductList').slideUp(500);
+                $('.unChosenLi').slideDown(500);
                 setTimeout(function () {flag = !flag;},500);
             }
         });
